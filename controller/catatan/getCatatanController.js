6a115e78314fd8a4373b8cca891433be6bad9f27
Fikri Maulana <maulanafikri455@gmail.com>
2023-06-21 20:54:19 +0700

    add models folder

--- controller/catatan/getCatatanController.js
+++ controller/catatan/getCatatanController.js
@@ -1,20 +1,20 @@
 const db = require("../../config/db"); // Impor file database.js
 
 const getCatatan = (req, res) => {
-	db.query("SELECT * FROM catatan", (error, results) => {
-		if (error) {
-			console.error(error);
-			return res.status(500).json({ message: "Terjadi kesalahan." });
-		}
-		if (results.length === 0) {
-			return res.status(404).json({ status: "Data Tidak Ada", data: [] });
-		}
-		return res.status(200).json({
-			status: "Success",
-			message: "Berhasil Mengambil Catatan!",
-			data: results,
-		});
-	});
+	catatanModel.getCatatan((error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ status:'Failed', message: 'Terjadi kesalahan.' });
+    }
+    if (results.length === 0) {
+      return res.status(404).json({ status: 'Failed', message:'Data tidak ada!', data: [] });
+    }
+    return res.status(200).json({
+      status: 'Success',
+      message: 'Berhasil Mengambil Catatan!',
+      data: results,
+    });
+  });
 };
 
 module.exports = getCatatan
