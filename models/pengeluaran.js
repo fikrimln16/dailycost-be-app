2eb2d46931e0e15f11bbf120328a0a7bfc8bd25a
Fikri Maulana <maulanafikri455@gmail.com>
2023-06-22 08:26:30 +0700

    add deletePengeluaran models

---
+++ models/pengeluaran.js
@@ -0,0 +1,14 @@
+const db = require("../config/db");
+
+const pengeluaranModel = {
+  deletePengeluaran: (user_id, pengeluaran_id, callback) => {
+    db.query("SELECT jumlah, pembayaran FROM pengeluaran WHERE user_id = ? AND pengeluaran_id = ?", [user_id, pengeluaran_id], (error, result) => {
+      if(error){
+        return callback(error, null);
+      }
+      return callback(null, result)
+    })
+  }
+}
+
+module.exports = pengeluaranModel
