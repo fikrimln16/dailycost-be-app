f916fe08585d8ab6831850cd378dfea9fc74baa3
Fikri Maulana <maulanafikri455@gmail.com>
2023-06-24 13:55:11 +0700

    edit postPemasukkan docs

--- controller/pemasukan/postPemasukanController.js
+++ controller/pemasukan/postPemasukanController.js
@@ -46,24 +46,25 @@ const pemasukanModel = require("../../models/pemasukan");
  *               properties:
  *                 status:
  *                   type: string
+ *                   example : Succes
  *                 message:
  *                   type: string
+ *                   example: Berhasil memasukkan pemasukan
  *                 data:
  *                   type: object
  *                   properties:
- *                     nama:
- *                       type: string
- *                     tanggal:
- *                       type: string
- *                       format: date
- *                     jumlah:
- *                       type: number
- *                     pembayaran:
- *                       type: string
  *                     user_id:
+ *                       type: integer
+ *                       example: 73
+ *                     uang_gopay:
  *                       type: number
- *                     kategori:
- *                       type: string
+ *                       example: 20000
+ *                     uang_cash:
+ *                       type: number
+ *                       example: 0
+ *                     uang_rekening:
+ *                       type: number
+ *                       example: 0
  *       400:
  *         description: Terjadi kesalahan input
  *         content:
@@ -79,11 +80,27 @@ const pemasukanModel = require("../../models/pemasukan");
  *               error1:
  *                 value:
  *                   status: Failed
- *                   message: Terjadi kesalahan pada input, mohon masukkan data yang benar!
+ *                   message: Terjadi kesalahan pada input, mohon masukkan data yang benar!, Metode pembayaran tidak valid, mohon masukkan data seperti `GOPAY`, `CASH`, atau `REKENING`
  *               error2:
  *                 value:
  *                   status: Failed
  *                   message: Metode pembayaran tidak valid, mohon masukkan data seperti `GOPAY`, `CASH`, atau `REKENING`.
+ *       401:
+ *         description: Terjadi kesalahan input user_id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 message:
+ *                   type: string
+ *             examples:
+ *               error1:
+ *                 value:
+ *                   status: Failed
+ *                   message: Pastikan masukkan user_id yang benar
  *       500:
  *         description: Terjadi kesalahan pada server saat melakukan pembelanjaan
  *         content:
@@ -97,7 +114,7 @@ const pemasukanModel = require("../../models/pemasukan");
  *                   type: string
  *             example:
  *               status: Failed
- *               message: Terjadi kesalahan pada server saat melakukan pembelanjaan.
+ *               message: Terjadi kesalahan pada server saat melakukan pemasukan.
  */
 const pemasukan = (req, res) => {
   const { nama, tanggal, jumlah, pembayaran, user_id, kategori } = req.body;
@@ -106,7 +123,7 @@ const pemasukan = (req, res) => {
     if(statusCode==400){
       return res.status(400).json({
         status: 'Failed',
-        message: result,
+        message: "Terjadi kesalahan pada input, mohon masukkan data yang benar!, Metode pembayaran tidak valid, mohon masukkan data seperti `GOPAY`, `CASH`, atau `REKENING`"
       });
     }
 
@@ -120,7 +137,7 @@ const pemasukan = (req, res) => {
     if (error) {
       return res.status(500).json({
         status: 'Failed',
-        message: error,
+        message: "Terjadi kesalahan pada server saat melakukan pemasukan."
       });
     }
     
